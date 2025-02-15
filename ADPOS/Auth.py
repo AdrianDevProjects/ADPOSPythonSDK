be68aaa41c37b6ec82a7a32302866813c12ce835
Adrian Albrecht <adrianalbrecht08@gmail.com>
2025-02-15 15:09:03 +0100

    build v1.1.0

--- ADPOS/Auth.py
+++ ADPOS/Auth.py
@@ -5,7 +5,7 @@ def info():
 
 
 def login(username, password, return_mode):
-    url = "https://onlineservices.adriandevprojects.com/v1/auth/login/"
+    login_url = "https://onlineservices.adriandevprojects.com/v1/auth/login/"
 
     credentials = {
         "username": username,
@@ -16,7 +16,7 @@ def login(username, password, return_mode):
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
-    response = requests.post(url, data=credentials, headers=headers)
+    response = requests.post(login_url, data=credentials, headers=headers)
 
     if return_mode in ("content+code", "code+content"):
         return f"{response.status_code}\n{response.text}"
@@ -26,3 +26,28 @@ def login(username, password, return_mode):
         return response.status_code
     else:
         return "Invalid return configuration in request"
+
+
+def register(username, password, return_mode, instant_login=bool):
+    register_url = "https://onlineservices.adriandevprojects.com/v1/auth/register/"
+
+    register_credentials = {
+        "username": username,
+        "password": password,
+        "confirm_password": password
+    }
+
+    register_headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.post(register_url, data=register_credentials, headers=register_headers)
+
+    if response.status_code == 201:
+        if instant_login:
+            login(username, password, return_mode)
+    else:
+        return f"Registration failed: {response.text}"
+
+
+
---
+++ tests/registertest.py
@@ -0,0 +1,3 @@
+import ADPOS.Auth as Auth
+
+print(Auth.register("test1", "testpw", "code+content", True))
