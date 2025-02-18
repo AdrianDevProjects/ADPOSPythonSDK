cb28b61b6a665820ff8c784d39936666ea336302
Adrian Albrecht <adriandevprojects@gmail.com>
2025-02-18 12:43:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ADPOSPythonSDK",
-    version="1.1.0",
+    version="1.1.1_FINAL",
     author="Adrian Albrecht",
     author_email="adriandevprojects@gmail.com",
     packages=setuptools.find_packages(),
