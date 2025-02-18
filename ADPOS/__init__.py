e64b2888a1740202b133eec3d70704de53485f84
Adrian Albrecht <adriandevprojects@gmail.com>
2025-02-18 12:45:26 +0100

    Update __init__.py

--- ADPOS/__init__.py
+++ ADPOS/__init__.py
@@ -1,2 +1,2 @@
 def info():
-    print("AdrianDevProjects Online Services Python SDK v1.1.0 by Adrian Albrecht")
+    print("AdrianDevProjects Online Services Python SDK v1.1.1_FINAL by Adrian Albrecht")
